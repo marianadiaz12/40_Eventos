685baa160f4efcf0b4dbb2ebe0456702f1609828
marianadiaz12 <mariana.diazes@gmail.com>
2016-10-21 16:52:00 -0500

    Twitter2

---
+++ Twitter/js/main.js
@@ -0,0 +1,27 @@
+function newElement() {
+    var li = document.createElement("li");
+    var textValue = document.getElementById("tarea").value;
+    var t = document.createTextNode(textValue);
+    li.appendChild(t);
+    if (textValue === '') {
+        alert("No puedes agregar una tarea vacia");
+    } else {
+        document.getElementById("tareas").appendChild(li);
+    }
+    document.getElementById("tarea").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    var close = document.getElementsByClassName("close");
+    var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+            }
+        }
+}

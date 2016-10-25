a92c3f718dc673781e4daaba2120fa8ae5397a0a
marianadiaz12 <mariana.diazes@gmail.com>
2016-10-25 09:00:30 -0500

    Twitter

--- Twitter/js/main.js
+++ Twitter/js/main.js
@@ -10,25 +10,25 @@ function newElement() {
     }
     document.getElementById("tarea").value = "";
 
-    // var span = document.createElement("SPAN");
-    // var txt = document.createTextNode("\u00D7");
-    // span.className = "close";
-    // span.appendChild(txt);
-    // li.appendChild(span);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
 
-    // var close = document.getElementsByClassName("close");
-    // var i;
-    //     for (i = 0; i < close.length; i++) {
-    //         close[i].onclick = function() {
-    //         var div = this.parentElement;
-    //         div.style.display = "none";
-    //         }
-    //     }
+    var close = document.getElementsByClassName("close");
+    var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+            }
+        }
 
-    // var list = document.querySelector('ul');// El check subrayado
-    // list.addEventListener('click', function(ev)  {
-    // if (ev.target.tagName === 'LI') {
-    //     ev.target.classList.toggle('checked');
-    //     }
-    // }, false);
+    var list = document.querySelector('ul');// El check subrayado
+    list.addEventListener('click', function(ev)  {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+        }
+    }, false);
 }
